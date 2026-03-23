45b50a4f81bfd2d55bf625551779d0738447df0c
AttractAcquisition <attractacquisition@gmail.com>
2026-03-23 13:36:05 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // This ensures paths work on your custom domain
+  base: '', // Using an empty string makes paths "completely relative"
 })
